0023710a6ff2f07a20bb1bee6c878dccdbd0f9c6
George Treneff <treneff@protonmail.com>
2022-12-16 16:21:29 +0000

    :package: added router and setup transitions

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
 import Homepage from './containers/Homepage';
 import Header from './components/Header';
+import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 function App() {
+    const location = useLocation();
+    const [displayLocation, setDisplayLocation] = useState(location);
+    const [transitionStage, setTransitionStage] = useState('fadeIn');
+
+    useEffect(() => {
+        if (location !== displayLocation) setTransitionStage('fadeOut');
+    }, [location, displayLocation]);
+
     return (
         <div className='App'>
             <Header />
-            <Homepage />
+
+            <Homepage
+                location={location}
+                displayLocation={displayLocation}
+                setDisplayLocation={setDisplayLocation}
+                transitionStage={transitionStage}
+                setTransitionStage={setTransitionStage}
+            />
         </div>
     );
 }
