bfed59514e25452949a6ba3c20ecebfdd0db287a
George Treneff <treneff@protonmail.com>
2022-12-17 10:42:22 +0000

    play button skeleton

--- client/src/components/PlayGameButton.js
+++ client/src/components/PlayGameButton.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Canvas from './game/Canvas';
-
+import { Link } from 'react-router-dom';
 const PlayGameButton = () => {
     return (
-        <div>
-            <Canvas />
-        </div>
+        <Link to="/game">
+        Play a game
+        </Link>
     );
 };
 
