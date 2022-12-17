9071c6772dbe5a5e84f6bd89cd8f283a4cb46791
George Treneff <treneff@protonmail.com>
2022-12-17 12:04:00 +0000

    updated header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import {Link} from "react-router-dom"
 
 const Header = () => {
-    return <header><div>Home Button</div> <h1>Space Saga</h1></header>;
+    return <header><Link to="/">Home Button</Link> <h1>Space Saga</h1></header>;
 };
 
 export default Header;
