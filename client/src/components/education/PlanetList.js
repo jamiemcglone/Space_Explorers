c84db50c1b1a4ae0dd2218069801f0ef6542edea
jamiemcglone <jamie.mcglone21@btinternet.com>
2022-12-18 11:01:50 +0000

    Formatted the planet nav bar so it is horizontal

--- client/src/components/education/PlanetList.js
+++ client/src/components/education/PlanetList.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Planet from './Planet';
-import { useState } from 'react';
+// import { useState } from 'react';
+import styled from "styled-components";
+
 
 const PlanetList = ({ planets }) => {
     const listOfPlanets = planets.map((planet, index) => {
         return <Planet key={index} planet={planet} />;
     });
 
-    return <ul>{listOfPlanets}</ul>;
+    return <PlanetNavBar>{listOfPlanets}</PlanetNavBar>;
 };
+
+const PlanetNavBar = styled.ul`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    list-style: none;
+`
 
 export default PlanetList;
