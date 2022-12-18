528df354a9cf35d51dff6875e3ae5f93eb7ddc3c
George Treneff <treneff@protonmail.com>
2022-12-18 20:38:24 +0000

    :construction: added moving rocket & interraction with planets

--- client/src/components/game/Canvas.js
+++ client/src/components/game/Canvas.js
@@ -2,32 +2,50 @@ import React from 'react';
 import QuizPlanetList from './QuizPlanetList';
 import { useState, useEffect } from 'react';
 import './Canvas.css';
+import PlanetService from '../../service/PlanetService';
 
 const Canvas = () => {
     const [x, setX] = useState(0);
     const [y, setY] = useState(0);
+    const [questionToDisplay, setQuestionToDisplay] = useState('');
     const planet1Position = { x: 0, y: 0 };
-    const planet2Position = { x: 500, y: 500 };
+    const planet2Position = { x: 150, y: 150 };
+    const planet3Position = { x: 500, y: 500 };
+
+    const [allPlanets, setAllPlanets] = useState([
+        { name: 'Venus', coordinate: { x: 0, y: 0 }, question: 'question venus' },
+        { name: 'Mercury', coordinate: { x: 150, y: 150 }, question: 'question Mercury' },
+        { name: 'mars', coordinate: { x: 500, y: 500 }, question: 'question Mars' },
+    ]);
+
     function onKeyDown(e) {
         e.preventDefault();
         if (e.key === 'ArrowUp') {
-            setY(y - 25);
+            if(y>0 ){setY(prevState => prevState - 10)};
+            setQuestionToDisplay('');
         } else if (e.key === 'ArrowDown') {
-            setY(y + 25);
+            if(y<950){setY(prevState => prevState + 10)};
+            setQuestionToDisplay('');
         } else if (e.key === 'ArrowLeft') {
-            setX(x - 25);
+            if(x>0){setX(prevState => prevState - 10);}
+            setQuestionToDisplay('');
         } else if (e.key === 'ArrowRight') {
-            setX(x + 25);
+            if(x<950){setX(prevState => prevState + 10);}
+            setQuestionToDisplay('');
         } else if (e.key === ' ') {
-            openPlanet(x, y);
+            openPlanet();
         }
     }
-    const openPlanet = (xClicked, yClicked) => {
-        if (planet1Position.x === xClicked && planet1Position.y === yClicked) {
-            console.log('succeed');
-        } else if (planet2Position.x === xClicked && planet2Position.y === yClicked) {
-            console.log('succeed 2');
-        }
+
+    const handleQuestion = () => {
+        setQuestionToDisplay('');
+    };
+    const openPlanet = () => {
+        return allPlanets.map((planet) => {
+            if (x === planet.coordinate.x) {
+                setQuestionToDisplay(planet.question);
+            }
+        });
     };
     const playerStyle = {
         left: x,
@@ -42,11 +60,28 @@ const Canvas = () => {
         left: planet2Position.x,
         top: planet2Position.y,
     };
+    const planet3Style = {
+        left: planet3Position.x,
+        top: planet3Position.y,
+    };
+
     return (
         <div tabIndex='0' onKeyDown={onKeyDown} className='game-container'>
             <div className='player' style={playerStyle} />
-            <div className='planet' style={planet1Style}></div>
-            <div className='planet' style={planet2Style}></div>
+            <div className='planet' style={planet1Style}>
+                Venus
+            </div>
+            <div className='planet' style={planet2Style}>
+                {' '}
+                mercury
+            </div>
+            <div className='planet' style={planet3Style}>
+                Mars
+            </div>
+            {questionToDisplay?<div className='question'>
+                {questionToDisplay}
+                <button onClick={handleQuestion}></button>
+            </div>:null}
         </div>
     );
 };
