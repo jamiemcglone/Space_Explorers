502c0f7ff7c2e4a8ffaadde09f2e91af64db51c5
jamiemcglone <jamie.mcglone21@btinternet.com>
2022-12-18 12:37:28 +0000

    Added comments outlining possible way to fix the refresh issue, seems longwinded though

--- client/src/components/education/PlanetInfo.js
+++ client/src/components/education/PlanetInfo.js
@@ -1,9 +1,27 @@
+// import { useEffect, useState, useCallback } from "react";
 import React from 'react';
 
 const PlanetInfo = (planet) => {
-    /// generate info about the planet
-    console.log(planet.planet.image);
+// Trying to solve state persistance, may be a long winded solution
+// This would take in the entire planets instead of planet
 
+    /// generate info about the planet
+    // console.log(planet.planet.image);
+    // console.log(planetToDisplay)
+
+    // const url = window.location.href
+    // const splitURL = url.split('/')
+    // const planetFromURL = splitURL.slice(-1)[0]
+    // console.log(planetFromURL)
+
+    // const [planetToDisplay, setPlanetToDisplay] = useState({})
+    // for (planet of planets) {
+        // if planet.planet.name === planetFromURL (
+            // setPlanetToDisplay(planet.planet)
+        // )
+    // }
+
+    // Set the return function to be planetToDisplay
     return (
         <div>
             <h3>{planet.name}</h3>
@@ -11,6 +29,7 @@ const PlanetInfo = (planet) => {
             <img src={planet.planet.image} alt="planet" />
         </div>
     );
-};
+
+}
 
 export default PlanetInfo;
