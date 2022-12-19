e7a1f15451d035f04f0935dc017d576e45153c2f
Olive-kaa <olive.kaa@gmail.com>
2022-12-19 16:52:58 +0000

    added question component

--- client/src/components/game/Question.js
+++ client/src/components/game/Question.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import FeedbackQuestion from './FeedbackQuestion';
-const Question = () => {
+import { Link } from 'react-router-dom';
+
+const Question = ({ question }) => {
+
+    const handleAnswer = (userAnswer, question) => {
+        if (userAnswer == {correctAnswer})
+                console.log('Well done, your answer is correct');
+            else; 
+                console.log("The correct answer was `${correctAnswer}")
+      }
+
     return (
-        <div>
-            A question and then <FeedbackQuestion />
+        <div className="question">
+
+            <Link to={'/${questions}'}>{question}
+                <ul>
+                    <li>{question.option.a}</li>
+                    <li>{question.option.b}</li>
+                    <li>{question.option.c}</li>
+                </ul>
+            </Link>
+            <button type="button" className="answer-btn"
+            onClick={handleAnswer}
+            >Check your answer</button>
+
+            <FeedbackQuestion />
         </div>
     );
 };
