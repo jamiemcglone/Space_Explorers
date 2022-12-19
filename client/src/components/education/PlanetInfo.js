db74819f5f12edf23d41b2e962310c8da573fc22
jamiemcglone <jamie.mcglone21@btinternet.com>
2022-12-19 16:11:14 +0000

    Fixed the refresh error

--- client/src/components/education/PlanetInfo.js
+++ client/src/components/education/PlanetInfo.js
@@ -1,30 +1,12 @@
-// import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState, useCallback, useRef } from "react";
+// import {useHistory} from "react-router-dom";
 import React from 'react';
 import Styled from 'styled-components';
 import'./PlanetInfo.css';
 
-const PlanetInfo = (planet) => {
-// Trying to solve state persistance, may be a long winded solution
-// This would take in the entire planets instead of planet
+const PlanetInfo = ({planet}) => {
 
-    /// generate info about the planet
-    // console.log(planet.planet.image);
-    // console.log(planetToDisplay)
-
-    // const url = window.location.href
-    // const splitURL = url.split('/')
-    // const planetFromURL = splitURL.slice(-1)[0]
-    // console.log(planetFromURL)
-
-
-    // const [planetToDisplay, setPlanetToDisplay] = useState({})
-    // for (planet of planets) {
-        // if planet.planet.name === planetFromURL (
-            // setPlanetToDisplay(planet.planet)
-        // )
-    // }
-
-    // Set the return function to be planetToDisplay
+    if (!planet) return null;
 
     const Div0 = Styled.div`
     display: flex;
@@ -51,31 +33,30 @@ const PlanetInfo = (planet) => {
     return (
         <>
         <Div0>
-            <h3>{planet.planet.name}</h3>
-            <p>{planet.index}</p>
+            <h3>{planet.name}</h3>
             <Div1 className='planetInfo1'>
-                <img src={planet.planet.image} alt="planet" /> 
+                <img src={planet.image} alt="planet" /> 
                 <div>
-                <h4>{planet.planet.name}</h4>
-                <p>{planet.planet.general_info}</p> 
+                <h4>{planet.name}</h4>
+                <p>{planet.general_info}</p> 
                 </div>
             </Div1>
             <Div2>
-                <img src={planet.planet.image} alt="planet" />  
+                <img src={planet.image} alt="planet" />  
             </Div2>
-            </Div0>
-            <Div3>
+         </Div0>
+         <Div3>
                 <ul id="accordion">
                     <li>
-                        <label for="first">Size, Mass, Gravity <span>&#8595;</span></label>
-                        <input type="radio" name="accordion" id="first" checked/>
+                        <label htmlFor="first">Size, Mass, Gravity <span>&#8595;</span></label>
+                        <input type="radio" name="accordion" id="first" htmlChecked/>
                         <div className="content">
                             <p>Its very big and heavy and draws things to it
                             </p>
                         </div>
                     </li>
                     <li>
-                        <label for="second">Composition <span>&#8595;</span></label>
+                        <label htmlFor="second">Composition <span>&#8595;</span></label>
                         <input type="radio" name="accordion" id="second"/>
                         <div className="content">
                             <p>It is made of rock
@@ -83,7 +64,7 @@ const PlanetInfo = (planet) => {
                         </div>
                     </li>
                     <li>
-                        <label for="third">Would you like to know more? <span>&#8595;</span></label>
+                        <label htmlFor="third">Would you like to know more? <span>&#8595;</span></label>
                         <input type="radio" name="accordion" id="third"/>
                         <div className="content">
                             <p>I could know more but I do not know if I can...
