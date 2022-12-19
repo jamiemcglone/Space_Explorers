6d050a660165139529fe9661b21729d0c2c88c5b
George Treneff <treneff@protonmail.com>
2022-12-19 13:32:34 +0000

    corrected animation height

--- client/src/App.js
+++ client/src/App.js
@@ -15,36 +15,31 @@ function App() {
 
     console.log(allPlanets);
     return (
-        <div className='App'>
-            <Header />
-
+        <>
+            <div className='App'>
+                <Header />
+                <Routes>
+                    <Route path='/' element={<HomepageContainer planets={allPlanets} />} />
+                    <Route path='/game' element={<GameContainer />} />
+                    <Route path='/sun' element={<PlanetInfo planet={allPlanets[0]} />} />
+                    <Route path='/mercury' element={<PlanetInfo planet={allPlanets[1]} />} />
+                    <Route path='/venus' element={<PlanetInfo planet={allPlanets[2]} />} />
+                    <Route path='/earth' element={<PlanetInfo planet={allPlanets[3]} />} />
+                    <Route path='/mars' element={<PlanetInfo planet={allPlanets[4]} />} />
+                    <Route path='/jupiter' element={<PlanetInfo planet={allPlanets[5]} />} />
+                    <Route path='/saturn' element={<PlanetInfo planet={allPlanets[6]} />} />
+                    <Route path='/uranus' element={<PlanetInfo planet={allPlanets[7]} />} />
+                    <Route path='/neptune' element={<PlanetInfo planet={allPlanets[8]} />} />
+                    <Route path='/pluto' element={<PlanetInfo planet={allPlanets[9]} />} />
+                    <Route path='/pluto' element={<PlanetInfo planet={allPlanets[9]} />} />
+                </Routes>
+            </div>
             <div className='wrapper-animation'>
                 <div id='stars'></div>
                 <div id='stars2'></div>
                 <div id='stars3'></div>
-                <div id='title'>
-                    <span>JJOGSA</span>
-                    <br></br>
-                    <span>An exploration education!</span>
-                </div>
             </div>
-
-            <Routes>
-                <Route path='/' element={<HomepageContainer planets={allPlanets} />} />
-                <Route path='/game' element={<GameContainer />} />
-                <Route path='/sun' element={<PlanetInfo planet={allPlanets[0]} />} />
-                <Route path='/mercury' element={<PlanetInfo planet={allPlanets[1]} />} />
-                <Route path='/venus' element={<PlanetInfo planet={allPlanets[2]} />} />
-                <Route path='/earth' element={<PlanetInfo planet={allPlanets[3]} />} />
-                <Route path='/mars' element={<PlanetInfo planet={allPlanets[4]} />} />
-                <Route path='/jupiter' element={<PlanetInfo planet={allPlanets[5]} />} />
-                <Route path='/saturn' element={<PlanetInfo planet={allPlanets[6]} />} />
-                <Route path='/uranus' element={<PlanetInfo planet={allPlanets[7]} />} />
-                <Route path='/neptune' element={<PlanetInfo planet={allPlanets[8]} />} />
-                <Route path='/pluto' element={<PlanetInfo planet={allPlanets[9]} />} />
-                <Route path='/pluto' element={<PlanetInfo planet={allPlanets[9]} />} />
-            </Routes>
-        </div>
+        </>
     );
 }
 
