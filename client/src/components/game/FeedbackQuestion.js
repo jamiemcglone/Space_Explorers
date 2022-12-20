2a909112ec1dcbc98ae05c17d3166d08d1960632
Olive-kaa <olive.kaa@gmail.com>
2022-12-20 11:04:36 +0000

    question and feedback components updated so the player can answer a question

--- client/src/components/game/FeedbackQuestion.js
+++ client/src/components/game/FeedbackQuestion.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const FeedbackQuestion = () => {
-    return <div><h1>Great Answer! you answer was correct/incorrect</h1> <button>Continue the game</button></div>;
+const FeedbackQuestion = ({ userAnswer, correctAnswer, extension, options } ) => {
+    let message = " "
+    if (userAnswer === correctAnswer){
+        message = `Well done you, that's correct! ${extension}`;
+    }
+    else {
+        message = `Sorry, the correct answer is ${options[correctAnswer]}. ${extension}`;
+    }
+    return <div>
+        <h1>{message}</h1>
+
+        <button>Continue the game</button>
+        </div>;
 };
 
 export default FeedbackQuestion;
--- client/src/components/game/Question.js
+++ client/src/components/game/Question.js
@@ -1,33 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FeedbackQuestion from './FeedbackQuestion';
-import { Link } from 'react-router-dom';
 
 const Question = ({ questionToDisplay }) => {
-    console.log(questionToDisplay);
 
-    // const handleAnswer = (userAnswer, questionToDisplay) => {
-    //     if (userAnswer === {correctAnswer})
-    //             console.log('Well done, your answer is correct');
-    //         else; 
-    //             console.log("The correct answer was `${correctAnswer}")
-    //   }
+    const [userAnswer, setUserAnswer] = useState(null)
+    
+    const handleUserAnswer = (event) => {
+        event.preventDefault()
+        setUserAnswer(event.target.answer.value)
+      }
 
     return (
         <div className="question">
+            <p>We are in question</p>
+            <form onSubmit={handleUserAnswer}>
+                <div>{questionToDisplay.question}
+                    <input type={"radio"} id={questionToDisplay.options.a} name="answer" value='a' />
+                    <label htmlFor={questionToDisplay.options.a}>{questionToDisplay.options.a}</label>
+                    <input type={"radio"} id={questionToDisplay.options.b} name="answer" value='b' />
+                    <label htmlFor={questionToDisplay.options.b}>{questionToDisplay.options.b}</label>
+                    <input type={"radio"} id={questionToDisplay.options.c} name="answer" value='c'/>
+                    <label htmlFor={questionToDisplay.options.c}>{questionToDisplay.options.c}</label>
+                </div>
 
-            <div>{questionToDisplay.question}
-                <ul>    
-                {/* option instead of list */}
-                    <li>{questionToDisplay.options.a}</li>
-                    <li>{questionToDisplay.options.b}</li>
-                    <li>{questionToDisplay.options.c}</li>
-                </ul>
-            </div>
-            <button type="button" className="answer-btn"
-            // onClick={handleAnswer}
-            >Check your answer</button>
-
-            <FeedbackQuestion />
+                <button className="answer-btn">
+                    Check your answer
+                </button> 
+            </form>
+            {userAnswer ? 
+                <FeedbackQuestion userAnswer={userAnswer} correctAnswer={questionToDisplay.correctAnswer} extension={questionToDisplay.extension} options={questionToDisplay.options}/> : null
+            }
         </div>
     );
 };
