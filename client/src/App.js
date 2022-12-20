d91cad796e4b44b80364de4fa2e313328aa660d8
jamiemcglone <jamie.mcglone21@btinternet.com>
2022-12-20 12:31:18 +0000

    Corrected routes and implemented random number generator on planet info

--- client/src/App.js
+++ client/src/App.js
@@ -22,17 +22,18 @@ function App() {
                 <Routes>
                     <Route path='/' element={<HomepageContainer planets={allPlanets} />} />
                     <Route path='/game' element={<GameContainer planets={allPlanets} />} />
-                    <Route path='/sun' element={<PlanetInfo planet={allPlanets[0]} />} />
-                    <Route path='/mercury' element={<PlanetInfo planet={allPlanets[1]} />} />
-                    <Route path='/venus' element={<PlanetInfo planet={allPlanets[2]} />} />
-                    <Route path='/earth' element={<PlanetInfo planet={allPlanets[3]} />} />
-                    <Route path='/mars' element={<PlanetInfo planet={allPlanets[4]} />} />
-                    <Route path='/jupiter' element={<PlanetInfo planet={allPlanets[5]} />} />
-                    <Route path='/saturn' element={<PlanetInfo planet={allPlanets[6]} />} />
-                    <Route path='/uranus' element={<PlanetInfo planet={allPlanets[7]} />} />
-                    <Route path='/neptune' element={<PlanetInfo planet={allPlanets[8]} />} />
-                    <Route path='/pluto' element={<PlanetInfo planet={allPlanets[9]} />} />
+                    <Route path='/sun' element={allPlanets[0] ? <PlanetInfo planet={allPlanets[0]} /> : null} />
+                    <Route path='/mercury' element={allPlanets[1] ?<PlanetInfo planet={allPlanets[1]}/> : null} />
+                    <Route path='/venus' element={allPlanets[2] ? <PlanetInfo planet={allPlanets[2]} /> : null} />
+                    <Route path='/earth' element={allPlanets[3] ? <PlanetInfo planet={allPlanets[3]} /> : null} />
+                    <Route path='/mars' element={allPlanets[4] ? <PlanetInfo planet={allPlanets[4]} /> : null} />
+                    <Route path='/jupiter' element={allPlanets[5] ? <PlanetInfo planet={allPlanets[5]} /> : null} />
+                    <Route path='/saturn' element={allPlanets[6] ? <PlanetInfo planet={allPlanets[6]} /> : null} />
+                    <Route path='/uranus' element={allPlanets[7] ? <PlanetInfo planet={allPlanets[7]} /> : null} />
+                    <Route path='/neptune' element={allPlanets[8] ? <PlanetInfo planet={allPlanets[8]} /> : null} />
+                    <Route path='/pluto' element={allPlanets[9] ? <PlanetInfo planet={allPlanets[9]} /> : null} />
                 </Routes>
+
             </div>
             <div className='wrapper-animation'>
                 <div id='stars'></div>
--- client/src/components/education/PlanetInfo.js
+++ client/src/components/education/PlanetInfo.js
@@ -5,7 +5,13 @@ import styled from 'styled-components';
 import'./PlanetInfo.css';
 
 const PlanetInfo = ({planet}) => {
-    if (!planet) return null;
+
+    const [randomIndex, setRandomIndex] = useState(0);
+    useEffect(() => {
+        handleClick()
+    }, [randomIndex])
+
+    // if (!planet) return null
 
     const Div0 = styled.div`
     display: flex;
@@ -34,14 +40,31 @@ const PlanetInfo = ({planet}) => {
         width:auto;
         
     `
+    const array = [1, 2, 3, 4, 5];
 
-    
+
+    // let randomNumber;
+    // useEffect(() => {
+
+    // }, [randomNumber])
+    const handleClick = () => {
+        
+        const getRandomNumber = () => {
+            const random = Math.floor(Math.random() * array.length);
+            return random;
+        }
+        
+        setRandomIndex(array[getRandomNumber()]);
+
+    }
+
+    console.log(randomIndex);
 
 
 
     return (
         <>
-        <Div0>
+            <Div0>
             <h3>{planet.name}</h3>
             <Div1 className='planetInfo1'>
                 <img src={planet.image} alt="planet" /> 
@@ -51,42 +74,44 @@ const PlanetInfo = ({planet}) => {
                 </div>
             </Div1>
             <Div2>
+            <button onClick={handleClick}>Click me for a random image!</button>
+            <p>{array[randomIndex - 1]}</p>
+            {/* <img src={randomImageResult} alt="Random-Image" /> */}
             </Div2>
-         </Div0>
-         <Div3>
-                <ul id="accordion">
-                    <li>
-                        <label htmlFor="first">Size, Mass, Gravity <span>&#8595;</span></label>
-                        <input type="radio" name="accordion" id="first" htmlChecked/>
-                        <div className="content">
-                            <p>Size: {planet.data.size}<br></br>
-                            Mass: {planet.data.mass}<br></br>
-                            Gravity: {planet.data.gravity}<br></br>
-                            </p>
-                        </div>
-                    </li>
-                    <li>
-                        <label htmlFor="second">Composition <span>&#8595;</span></label>
-                        <input type="radio" name="accordion" id="second"/>
-                        <div className="content">
-                            <p>{planet.data.composition}
-                            </p>
-                        </div>
-                    </li>
-                    <li>
-                        <label htmlFor="third">Would you like to know more? <span>&#8595;</span></label>
-                        <input type="radio" name="accordion" id="third"/>
-                        <div className="content">
+            </Div0>
+            <Div3>
+            <ul id="accordion">
+            <li>
+            <label htmlFor="first">Size, Mass, Gravity <span>&#8595;</span></label>
+            <input type="radio" name="accordion" id="first" htmlChecked/>
+            <div className="content">
+            <p>Size: {planet.data.size}<br></br>
+            Mass: {planet.data.mass}<br></br>
+            Gravity: {planet.data.gravity}<br></br>
+            </p>
+            </div>
+            </li>
+            <li>
+            <label htmlFor="second">Composition <span>&#8595;</span></label>
+            <input type="radio" name="accordion" id="second"/>
+            <div className="content">
+            <p>{planet.data.composition}
+            </p>
+            </div>
+            </li>
+            <li>
+            <label htmlFor="third">Would you like to know more? <span>&#8595;</span></label>
+            <input type="radio" name="accordion" id="third"/>
+            <div className="content">
                             <p>{planet.data.extra}
                             </p>
                         </div>
                     </li>
-
-                </ul>
-               
-            </Div3>
-            </>
-        
+                    
+                    </ul>
+                    
+                    </Div3>
+                    </>
     );
 
 }
