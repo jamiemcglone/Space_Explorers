fb340fa1a46d1cf4efc15a15285337d90c5c634b
jamiemcglone <jamie.mcglone21@btinternet.com>
2022-12-21 12:12:30 +0000

    Leadboard displays in correct order, also added some css

--- client/src/components/Leaderboard.js
+++ client/src/components/Leaderboard.js
@@ -1,14 +1,35 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 const Leaderboard = ({ players }) => {
-    const playersToDisplay = players.map((player) => {
+
+    const LeadberboardContainer = styled.ol`
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;    
+        `
+
+const LeaderboardItem = styled.li`
+    justify-content: center;
+    list-style: none;
+    color: whitesmoke;
+    border: yellow 1px solid;
+    padding: 5px;
+    margin: 5px;
+    `
+    const sortedPlayers = players.sort((a, b) => (a.score < b.score) ? 1 : ((b.score < a.score) ? -1 : 0))
+
+    console.log(players);
+    console.log(sortedPlayers)
+    const playersToDisplay = sortedPlayers.map((player) => {
         return (
-            <li key={player._id}>
-                {player.name} {player.score}
-            </li>
+            <LeaderboardItem key={player._id}>
+                {player.name} - {player.score} points
+            </LeaderboardItem>
         );
     });
-    return <ol>{playersToDisplay}</ol>;
+    return <LeadberboardContainer>{playersToDisplay}</LeadberboardContainer>;
 };
 
 export default Leaderboard;
